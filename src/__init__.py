f864b659e3ab4b1361f9cd11b22d160525a105c4
DarFig <log.dariel@gmail.com>
2019-03-14 23:12:57 +0100

    estructura inicialç

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+data_base_uri = ""
+app.config['SQLAlCHEMY_DATABASE_URI'] = data_base_uri
+app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+import src.views
